2adaf1b0e80aa7ab876d3de45483a99bfbaa7ff0
ASUS <sd8g22@soton.ac.uk>
2024-03-12 03:40:25 +0000

    Update Icon

--- src/main/java/uk/ac/soton/comp2211/runwayredeclaration/Component/Runway.java
+++ src/main/java/uk/ac/soton/comp2211/runwayredeclaration/Component/Runway.java
@@ -7,50 +7,19 @@ import java.util.ArrayList;
 
 public class Runway {
 
-    private String designator;
+    private String name;
     private double length;
     private double width;
-    private double displacedThreshold;
-    private double stopwayLength;
-    private double clearwayLength;
-    private double blastProtection;
-    private SimpleDoubleProperty TORA;
-    private SimpleDoubleProperty TODA;
-    private SimpleDoubleProperty ASDA;
-    private SimpleDoubleProperty LDA;
-    private double RESA;
+
     private ArrayList< Pair<Obstacle, Double> > obstacleDistanceList;
-
-
+    private ArrayList<SubRunway> subRunways;
 
 
     public Runway (String name){
-        this.designator = name;
+        this.name = name;
     }
 
-//    public Runway (String name, double length, double width, double stopwayLength, double clearwayLength, double blastProtection){
-//        this.designator = name;
-//        this.length = length;
-//        this.width = width;
-//        this.displacedThreshold = 0;
-//        this.stopwayLength = stopwayLength;
-//        this.clearwayLength = clearwayLength;
-//        this.TORA = new SimpleDoubleProperty(length);
-//        this.TODA = new SimpleDoubleProperty(TORA.getValue() + clearwayLength) ;
-//        this.blastProtection = blastProtection;
-//
-//    }
 
-    public Runway (String name, double tora, double toda, double asda, double lda, int heading, double displacedThreshold, String position){
-        this.designator = name;
-        this.TORA = new SimpleDoubleProperty(tora);
-        this.TODA = new SimpleDoubleProperty(toda);
-        this.ASDA = new SimpleDoubleProperty(asda);
-        this.LDA = new SimpleDoubleProperty(lda);
-        this.displacedThreshold = displacedThreshold;
-        this.obstacleDistanceList = new ArrayList<>();
-        this.blastProtection = 300;
-    }
 
     /**
      * Add an obstacle to the runway
@@ -79,18 +48,18 @@ public class Runway {
 
     /**
      * Set the designator of the runway
-     * @param designator the name of the runway
+     * @param name the name of the runway
      */
-    public void setDesignator(String designator){
-        this.designator = designator;
+    public void setName(String name){
+        this.name = name;
     }
 
     /**
      * Get the designator of the runway
      * @return the name of the runway
      */
-    public String getDesignator(){
-        return designator;
+    public String getName(){
+        return name;
     }
 
     public void setLength(double length){
@@ -101,31 +70,6 @@ public class Runway {
         return length;
     }
 
-    public double getBlastProtection(){
-        return this.blastProtection;
-    }
-
-    public void setOriginalTORA(double TORA){
-        this.TORA.set(TORA);
-    }
-
-    public SimpleDoubleProperty getOriginalTORA(){
-        return this.TORA;
-    }
-
-    public SimpleDoubleProperty getOriginalTODA(){
-        return this.TODA;
-    }
-
-    public SimpleDoubleProperty getOriginalASDA(){
-        return this.ASDA;
-    }
-
-    public SimpleDoubleProperty getOriginalLDA(){
-        return this.LDA;
-    }
-
-
 
     public void setWidth(double width){
         this.width = width;
@@ -135,29 +79,6 @@ public class Runway {
         return width;
     }
 
-    public void setDisplacedThreshold(double displacedThreshold){
-        this.displacedThreshold = displacedThreshold;
-    }
-
-    public double getDisplacedThreshold(){
-        return displacedThreshold;
-    }
-
-    public void setStopwayLength(double stopwayLength){
-        this.stopwayLength = stopwayLength;
-    }
-
-    public double getStopwayLength(){
-        return stopwayLength;
-    }
-
-    public void setClearwayLength(double clearwayLength){
-        this.clearwayLength = clearwayLength;
-    }
-
-    public double getClearwayLength(){
-        return clearwayLength;
-    }
 
 
 }
